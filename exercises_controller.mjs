e2a347c68e855e9536084f8401d8215d3133332b
Dominic Fantauzzo <137939470+fantauzd@users.noreply.github.com>
2024-02-24 00:46:53 -0500

    Add files via upload

---
+++ exercises_controller.mjs
@@ -0,0 +1,105 @@
+import 'dotenv/config';
+import * as exercises from './exercises_model.mjs';
+import express from 'express';
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+
+/**
+ * Create a new exercise with the name, reps, weight, unit, and date provided in the body
+ */
+app.post('/exercises', (req, res) => {
+    exercises.createExercise(req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date)
+        .then(exercise => {
+            // res.setHeader('content-type', 'application/JSON');
+            res.status(201).json(exercise);
+        })
+        .catch(error => {
+            console.error(error);
+            // In case of an error, send back status code 400 in case of an error.
+            // A better approach will be to examine the error and send an
+            // error status code corresponding to the error.
+            res.status(400).json({ Error: 'Request failed' });
+        });
+});
+
+
+/**
+ * Retrive the exercise corresponding to the ID provided in the URL.
+ */
+app.get('/exercises/:_id', (req, res) => {
+    const exerciseId = req.params._id;
+    console.log(exerciseId);
+    exercises.findExerciseByID(exerciseId)
+        .then(exercise => { 
+            if (exercise !== null) {
+                res.status(200).json(exercise);
+                console.log('here');
+            } else {
+                res.status(404).json({ Error: 'Resource not found' });
+            }         
+         })
+        .catch(error => {
+            res.status(400).json({ Error: 'Request failed' });
+        });
+});
+
+/**
+ * Retrieve all exercises. 
+ * All exercises are returned as JSON in an array.
+ */
+app.get('/exercises', (req, res) => {
+    exercises.findExercises()
+        .then(exercise => {
+            res.status(200).send(exercise);
+        })
+        .catch(error => {
+            console.error(error);
+            res.send({ Error: 'Request failed' });
+        });
+});
+
+/**
+ * Update the exercise whose id is provided in the path parameter and set
+ * its name, reps, weight, unit, date to the values provided in the body.
+ */
+app.put('/exercises/:_id', (req, res) => {
+    exercises.replaceExercise(req.params._id, req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date)
+        .then(numUpdated => {
+            if (numUpdated === 1) {
+                res.status(200).json({ _id: req.params._id, name: req.body.name, reps: req.body.reps, weight: req.body.weight, unit: req.body.unit, date: req.body.date})
+            } else {
+                res.status(404).json({ Error: 'Resource not found' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(400).json({ Error: 'Request failed' });
+        });
+});
+
+/**
+ * Delete the exercise whose id is provided in the query parameters
+ */
+app.delete('/exercises/:_id', (req, res) => {
+    exercises.deleteById(req.params._id)
+        .then(deletedCount => {
+            if (deletedCount === 1) {
+                res.status(204).send();
+            } else {
+                res.status(404).json({ Error: 'Resource not found' });
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            res.send({ error: 'Request failed' });
+        });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
